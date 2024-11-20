48c9257cc81a25bfc8738138b06b386e35c8be51
Erie M. Adames <erieadames@gmail.com>
2024-11-19 22:29:46 -0500

    videoServer

--- videoServer.py
+++ videoServer.py
@@ -58,15 +58,19 @@ servo0.set_angle(servo0_angle_offset)
 servo0.set_angle(servo0_angle_offset)
 
 def up():
+    global servo1_angle_offset
     servo1_angle = max(servo1_angle - 2, -90)
     servo1.set_angle(servo1_angle + servo1_angle_offset)
 def down():
+    global servo1_angle_offset
     servo1_angle = min(servo1_angle + 2 , 90)
     servo1.set_angle(servo1_angle + servo1_angle_offset)
 def left():
+    global servo0_angle_offset
     servo0_angle = max(servo0_angle - 2, -90)
     servo0.set_angle(servo0_angle + servo0_angle_offset)
 def right():
+    global servo0_angle_offset
     servo0_angle = min(servo0_angle + 2, 90)
     servo0.set_angle(servo0_angle + servo0_angle_offset)
 
