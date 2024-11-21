a9e73be594c3b2f7b2f35d51fa473e64b7803043
Erie M. Adames <erieadames@gmail.com>
2024-11-20 23:49:58 -0500

    picam test server

--- videoServer.py
+++ videoServer.py
@@ -108,7 +108,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 def find_poop(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresholded = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    _, thresholded = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
     print('finding')
