ccdaf460d7c610e209e0dd257c849654ab0d845d
Erie M. Adames <erieadames@gmail.com>
2024-11-22 18:36:43 -0500

    picam test server

---
+++ motor.py
@@ -0,0 +1,30 @@
+from pwm import PWM
+from pin import Pin
+import time
+class Motor():
+    STEP = 10
+    DELAY = 0.1
+    def __init__(self, pwm_pin, dir_pin, is_reversed=False):
+        self.pwm_pin = pwm_pin
+        self.dir_pin = dir_pin
+        self._is_reversed = is_reversed
+        self._power = 0
+        self._except_power = 0
+    def set_power(self, power):
+        if power >= 0:
+            direction = 0
+        elif power < 0:
+            direction = 1
+        power = abs(power)
+        if power != 0:
+            power = int(power /2 ) + 50
+        power = power
+        direction = direction if not self._is_reversed else not direction  
+        self.dir_pin.value(direction)
+        self.pwm_pin.pulse_width_percent(power)
+
+m = Motor(PWM("P13"), Pin("D4"))
+while True:
+    time_ = int(input())
+    m.set_power(10)
+    time.sleep(time_)
--- servo.py
+++ servo.py
@@ -22,13 +22,12 @@ class Servo():
             angle = int(angle)
         except:
             raise ValueError("Angle value should be int value, not %s"%angle)
-        # if angle < -90:
-        #     angle = -90
-        # if angle > 90:
-        #     angle = 90
+        if angle < -90:
+            angle = -90
+        if angle > 90:
+            angle = 90
         angle = angle + self.offset
         High_level_time = self.mapping(angle, -90, 90, self.MIN_PW, self.MAX_PW)
-        print(High_level_time)
         pwr =  High_level_time / 20000
         value = int(pwr * self.PERIOD)
         self.pin.pulse_width(value)
