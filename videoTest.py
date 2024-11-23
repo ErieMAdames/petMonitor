e7220d10b202f95f3bb4359c9c960c54e7ad8bcc
Erie M. Adames <erieadames@gmail.com>
2024-11-23 14:54:14 -0500

    picam test server

--- videoTest.py
+++ videoTest.py
@@ -9,6 +9,7 @@ from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 from hailo_platform import (HEF, Device, VDevice, HailoStreamInterface, InferVStreams, ConfigureParams,
     InputVStreamParams, OutputVStreamParams, InputVStreams, OutputVStreams, FormatType)
+from yolo import YoloPostProc
 import logging
 
 PAGE = """\
@@ -26,6 +27,8 @@ PAGE = """\
 # Hailo Initialization
 hef = HEF("/usr/share/hailo-models/yolov6n.hef")  # Replace with your .hef model path
 
+INPUT_RES_H = 640
+INPUT_RES_W = 640
 
 def preprocess_frame(frame, input_shape):
     """Resize and normalize the frame for Hailo model input."""
@@ -105,16 +108,41 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
                         # Decode frame for processing
                         np_frame = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR)
-                        resized_img = cv2.resize(np_frame, (640, 640), interpolation = cv2.INTER_AREA)
+                        resized_img = cv2.resize(np_frame, (INPUT_RES_H, INPUT_RES_H), interpolation = cv2.INTER_AREA)
                         with InferVStreams(network_group, input_vstreams_params, output_vstreams_params) as infer_pipeline:
                             input_data = {input_vstream_info.name: np.expand_dims(np.asarray(resized_img), axis=0).astype(np.float32)}    
                             with network_group.activate(network_group_params):
                                 infer_results = infer_pipeline.infer(input_data)
                                 print(infer_results)
 
-                        # Overlay detections
-                        # processed_frame = overlay_detections(np_frame, detections)
+                        layer_from_shape: dict = {infer_results[key].shape:key for key in infer_results.keys()}            
+                        
+                        # postprocessing info for constructor as recommended in hailo_model_zoo/cfg/base/yolox.yaml
+                        anchors = {"strides": [32, 16, 8], "sizes": [[1, 1], [1, 1], [1, 1]]}
+                        yolox_post_proc = YoloPostProc(img_dims=(INPUT_RES_H,INPUT_RES_H), nms_iou_thresh=0.65, score_threshold=0.01, 
+                                                            anchors=anchors, output_scheme=None, classes=80, labels_offset=1, 
+                                                            meta_arch="yolox", device_pre_post_layers=[])                
 
+                        # Order of insertion matters since we need the reorganized tensor to be in (BS,H,W,85) shape
+                        endnodes = [infer_results[layer_from_shape[1, 80, 80, 4]],  # stride 8 
+                                    infer_results[layer_from_shape[1, 80, 80, 1]],  # stride 8 
+                                    infer_results[layer_from_shape[1, 80, 80, 80]], # stride 8 
+                                    infer_results[layer_from_shape[1, 40, 40, 4]],  # stride 16
+                                    infer_results[layer_from_shape[1, 40, 40, 1]],  # stride 16
+                                    infer_results[layer_from_shape[1, 40, 40, 80]], # stride 16
+                                    infer_results[layer_from_shape[1, 20, 20, 4]],  # stride 32
+                                    infer_results[layer_from_shape[1, 20, 20, 1]],  # stride 32
+                                    infer_results[layer_from_shape[1, 20, 20, 80]]  # stride 32
+                                ]
+                        hailo_preds = yolox_post_proc.yolo_postprocessing(endnodes)
+                        num_detections = int(hailo_preds['num_detections'])
+                        scores = hailo_preds["detection_scores"][0].numpy()
+                        classes = hailo_preds["detection_classes"][0].numpy()
+                        boxes = hailo_preds["detection_boxes"][0].numpy()
+                        if scores[0] == 0:
+                            num_detections = 0
+                        preds_dict = {'scores': scores, 'classes': classes, 'boxes': boxes, 'num_detections': num_detections}
+                        print(preds_dict)
                         self.wfile.write(b'--FRAME\r\n')
                         self.send_header('Content-Type', 'image/jpeg')
                         self.send_header('Content-Length', len(frame))
